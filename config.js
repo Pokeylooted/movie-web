3cc305f573d1e9946797210f366b9be2d557bd68
Pokey <79169880+Pokeylooted@users.noreply.github.com>
2023-09-30 11:47:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "movie-web.pokeyrandomgaming.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3M2VlMzdiMjYyNDAyYTExOWVkNzQwOGVjYTBhNWQzYiIsInN1YiI6IjY1MTg1MDg5OTZlMzBiMDBlMzM0ZWEwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1QyS7TDyqwqsrsFWm5G4fLKUx1L5NNU9FHQ3wcYoPsk"
 };
